6a21ea0e069cbd6859a92f7225fd9c2538ca8e32
pointdeveloper <pointdeveloper99@gmail.com>
2017-12-06 10:47:23 +0530

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+declare var navigator: any;
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  vibrate(){
+    navigator.vibrate(50)
+  }
 }
